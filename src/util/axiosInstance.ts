dd5eb3b6be3fb829cb3f2715f876032249c1a39a
Samir Prasad <samirprasad@Samirs-MacBook-Air.local>
2023-01-29 12:17:28 +0900

    added aio twsrts

--- src/util/axiosInstance.ts
+++ src/util/axiosInstance.ts
@@ -23,3 +23,42 @@ export const composeAxiosInstance = (requestInterceptor: RequestInterceptor) => 
 
   return axiosInstance;
 };
+
+export async function requestpostInfo(body: response, method: RequestMethod, options = {}, dryRun?: boolean) {
+  try {
+    const response = await requestData(url, {
+      ...options,
+      method,
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      timeout: 30000, // add timeout
+      softTimeout: 3000,
+      cache: 'no-cache', // add 'no-cache' for safe
+      credentials: 'same-origin',
+    });
+    return response;
+  } catch (e) {
+    throw e;
+  }
+}
+
+export async function getInfo(shopId: string, options = {}) {
+  // todo: refactor hard coding of shopId and change base_url(environment)
+  // URL : http://stg-rsitem-webapp.rwasp-stg.hnd2.bdd.local/shops/287705/shop-info
+  const url = `${BASE_URL}/test`;
+
+  try {
+    const response = await requestData(url, {
+      ...options,
+      method: 'get',
+      credentials: 'same-origin',
+      timeout: 60000,
+    });
+    const shopInfoData = await response.json();
+    return shopInfoData;
+  } catch (e) {
+    throw e;
+  }
+}
